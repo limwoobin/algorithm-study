fe2a6accc620daff1afcb8597b42cd9d3b055a4d
drogba <drogba02@naver.com>
2020-02-28 18:54:14 +0900

    No_1000

--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -7,29 +7,44 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String arr[] = br.readLine().split(" ");
-		int a = Integer.parseInt(arr[0]);
-		int b = Integer.parseInt(arr[1]);
-		int v = Integer.parseInt(arr[2]);
-		int cnt = 1;
-		int val = a - b;
 
-		v = v - a;
-		int su = v/val;
+		int cnt = Integer.parseInt(br.readLine());
 
-		if(v - (val * su) <= 0) {
-			cnt = cnt + su;
-		}else {
-			cnt = cnt + su + 1;
+		for(int i=0; i<cnt; cnt++) {
+			String[] arr = br.readLine().split(" ");
+			int height = Integer.parseInt(arr[0]);
+			int width = Integer.parseInt(arr[1]);
+			int num = Integer.parseInt(arr[2]);
+
+			String result = "";
+			int h = num / height;
+			if(h > 0 && (num % height) != 0) {
+				result = result.concat(Integer.toString(num % height));
+				if(h < 10) {
+					result = result.concat("0").concat(Integer.toString(h+1));
+				}else {
+					result = result.concat(Integer.toString(h+1));
+				}
+			}else {
+				if(height > width) {
+					result = result.concat(Integer.toString(num)).concat("01");
+				}else {
+					
+				}
+			}
+			System.out.println(result);
 		}
-		System.out.println(cnt);
-
-//		int su1 = a;
-//		while(su1 < v) {
-//			su1 = su1 - b;
-//			su1 = su1 + a;
-//			cnt++;
-//		}
-//		System.out.println(cnt);
 	}
 }
+
+
+//6 12 10 = 402
+//30 50 72 = 1203
+//
+//5 7 8 = 302
+//
+//5 8 8 = 302
+//
+//5 9 8 = 302
+//
+//10 5 6 = 601
