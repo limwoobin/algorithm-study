84e0d6745c1e6875cc6bcd0d033a6368cb877e63
drogba <drogba02@naver.com>
2020-03-12 18:31:16 +0900

    더하기 사이클

---
+++ src/Chapter1/No_1110.java
@@ -0,0 +1,36 @@
+package Chapter1;
+
+import java.util.Scanner;
+
+public class No_1110 {
+
+	public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
+	        int a = in.nextInt();
+	        int count = 0;
+	        int result = a;
+
+	        while(true) {
+	            count++;
+	            String temp;
+	            String[] temp2;
+	            if(a < 10) {
+	                temp = "0".concat(String.valueOf(a));
+	            }else {
+	                temp = String.valueOf(a);
+	            }
+	            temp2 = temp.split("");
+
+	            String lastNum = temp2[1];
+	            String lastHap = String.valueOf(Integer.parseInt(temp2[0]) + Integer.parseInt(temp2[1]));
+	            String nextNum = lastNum.concat(lastHap.substring(lastHap.length()-1, lastHap.length()));
+	            if(result == Integer.parseInt(nextNum)) {
+	                System.out.println(count);
+	                break;
+	            }else {
+	                a = Integer.parseInt(nextNum);
+	            }
+	        }
+	}
+
+}
--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -7,19 +7,28 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(System.in);
 
-		int sum = 1;
-		int pV = 0;
+		int[] arr = new int[6];
 
-		int k = in.nextInt();
-		int n = in.nextInt();
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int c = in.nextInt();
+		int d = in.nextInt();
+		int e = in.nextInt();
 
-		for(int i=0; i<k; i++) {
-			for(int j=1; j<=n; j++) {
-				sum = sum * j;
+		arr[0] = a + d;
+		arr[1] = a + e;
+		arr[2] = b + d;
+		arr[3] = b + e;
+		arr[4] = c + d;
+		arr[5] = c + e;
+
+		int min = arr[0];
+		for(int i=0; i<arr.length; i++) {
+			if(min > arr[i]) {
+				min = arr[i];
 			}
-			
 		}
 
-		System.out.println(sum);
+		System.out.println(min-50);
 	}
 }
