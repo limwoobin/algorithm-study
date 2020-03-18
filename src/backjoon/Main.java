b932e0995ffb4d8e52e794d9f4ac19573cfc0f9a
drogba <drogba02@naver.com>
2020-03-18 09:03:08 +0900

    quickSort Study...

--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -7,28 +7,27 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(System.in);
 
-		int[] arr = new int[6];
+		int num = in.nextInt();
+		int[] arr = new int[num];
+		int temp;
+		for(int i=0; i<num; i++) {
+			arr[i] = in.nextInt();
+		}
 
-		int a = in.nextInt();
-		int b = in.nextInt();
-		int c = in.nextInt();
-		int d = in.nextInt();
-		int e = in.nextInt();
-
-		arr[0] = a + d;
-		arr[1] = a + e;
-		arr[2] = b + d;
-		arr[3] = b + e;
-		arr[4] = c + d;
-		arr[5] = c + e;
-
-		int min = arr[0];
 		for(int i=0; i<arr.length; i++) {
-			if(min > arr[i]) {
-				min = arr[i];
+			for(int j=0; j<arr.length-1-i; j++) {
+				if(arr[j] > arr[j+1]) {
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
 			}
 		}
 
-		System.out.println(min-50);
+
+
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 }
---
+++ src/sort/QuickSort.java
@@ -0,0 +1,58 @@
+package sort;
+
+public class QuickSort {
+	public static void main(String[] args) {
+		// 분할 정복 알고리즘
+		// 1.pivot 값 정하기(보통 첫번째 자리)
+		// 2.왼쪽에서 부터 pivot값 보다 큰 값을 선택
+		// 3.오른쪽에서부터 pivot값 보다 작은 값을 선택
+		// 4. 2번값과 3번값 교환
+		// 5. 엇갈린 경우는 pivot값과 작은값을 교환
+		// 6. 분할이 되었음
+		// 7. 각 분할이 된 배열의 맨 앞자리가 pivot
+		// 8. 앞에서 했더 작업 다시 수행
+
+		int number = 10;
+		int[] arr = {3,7,8,1,5,9,6,10,2,4};
+		quickSort(arr , 0 , number-1);
+		for(int i=0; i<number; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+
+	public static void quickSort(int[] arr , int start, int end) {
+		if(start >= end) { // 원소가 1개인 경우
+			return;
+		}
+
+		int pivot = start; // 피벗값은 첫번째 원소
+		int i = start + 1;
+		int j = end;
+		int temp;
+
+		while(i <= j) { // 엇갈릴 때까지 반복
+			while(i <= end && arr[i] <= arr[pivot]) {
+				i++;
+			}
+			while(j > start && arr[j] >= arr[pivot]) {
+				j--;
+			}
+			if(i > j) {
+				temp = arr[j];
+				arr[j] = arr[pivot];
+				arr[pivot] = temp;
+			}else {
+				temp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = temp;
+			}
+		}
+
+		quickSort(arr , start , j-1);
+		quickSort(arr , j+1 , end);
+	}
+}
+
+// 시간복잡도 -> O(N*logN)
+// 최악의 복잡도는 O(N^2)
