ea4dc8b74d2cd50f7a69299fdc5ae2f91017f50c
drogba <drogba02@naver.com>
2020-04-08 09:28:51 +0900

    단어정렬 No_1881

--- src/backjoon/test.java
+++ src/backjoon/test.java
@@ -3,13 +3,12 @@ package backjoon;
 
 public class test {
 	public static void main(String[] args) {
-		int i = 0;
-		do {
-			System.out.println(i);
-			i++;
-		}while(i<=10);
+		String a = "asda";
+		String b = "fddgfr";
 
 
+		System.out.println(a.substring(0,1).hashCode());
+		System.out.println(b.substring(0,1).charAt(0));
 	}
 }
 
