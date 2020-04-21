13bb121562117cecc7f3752621d5005524fa6505
drogba <drogba02@naver.com>
2020-04-21 09:22:16 +0900

    퀵소트 스터디...

--- src/backjoon/test.java
+++ src/backjoon/test.java
@@ -9,7 +9,6 @@ public class test {
 		int[] arr = {3,1,8,7,2,5,4,6};
 		int number = arr.length;
 		int start = 0;
-
 		t.sort(arr , start , number-1);
 		for(int i=0; i<number; i++) {
 			System.out.print(arr[i] + " ");
@@ -17,11 +16,6 @@ public class test {
 	}
 
 	public void sort(int[] data , int l , int r) {
-		for(int i=0; i<data.length; i++) {
-			System.out.print(data[i] + " ");
-		}
-		System.out.println();
-
 		int left = l;
 		int right = r;
 		int pivot = data[(l+r) / 2];
@@ -37,8 +31,6 @@ public class test {
 				right--;
 			}
 		}while(left <= right);
-		System.out.println("l:" + l);
-		System.out.println("r:" + r);
 		if(l < right) sort(data , l ,right);
 		if(r > left) sort(data , left , r);
 	}
--- src/backjoon/test2.java
+++ src/backjoon/test2.java
@@ -2,11 +2,32 @@ package backjoon;
 
 public class test2 {
 	public static void main(String[] args) {
+		test2 t2 = new test2();
+		int[] arr = {3,1,5,7,6,8,2,4};
+		t2.quickSort(arr , 0 , arr.length-1);
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
 
-		int i=5;
+	public void quickSort(int data[] , int l , int r) {
+		int left = l;
+		int right = r;
+//		int pivot = data[(l + r) / 2];
+		int pivot = data[3];
+
 		do {
-			System.out.println(i);
-			i--;
-		}while(i>=1);
+			while(data[left] < pivot) left++;
+			while(data[right] > pivot) right--;
+			if(left <= right) {
+				int temp = data[left];
+				data[left] = data[right];
+				data[right] = temp;
+				left++;
+				right--;
+			}
+		}while(left <= right);
+		if(l < right) quickSort(data , l , right);
+		if(r > left) quickSort(data , left , r);
 	}
 }
