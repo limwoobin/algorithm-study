251e157013b26b44122d940f8aecc088883c8aa6
drogba <drogba02@naver.com>
2020-05-08 18:41:33 +0900

    숫자의 개수 No_2577

---
+++ src/Chapter1/No_2577.java
@@ -0,0 +1,28 @@
+package Chapter1;
+
+import java.util.Scanner;
+
+public class No_2577 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int[] res =  new int[10];
+
+        int result = a*b*c;
+        String stResult = String.valueOf(result);
+        for(int i=0; i<10; i++) {
+            int count = 0;
+            for(int j=0; j<stResult.length(); j++) {
+                if(String.valueOf(stResult.charAt(j)).equals(String.valueOf(i))) {
+                    count++;
+                }
+            }
+
+         System.out.println(count);
+
+        }
+    }
+}
--- src/backjoon/test.java
+++ src/backjoon/test.java
@@ -1,37 +1,18 @@
 package backjoon;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class test {
 	public static void main(String[] args) throws IOException {
 		test t = new test();
-
-		int[] arr = {3,1,8,7,2,5,4,6};
-		int number = arr.length;
-		int start = 0;
-		t.sort(arr , start , number-1);
-		for(int i=0; i<number; i++) {
-			System.out.print(arr[i] + " ");
-		}
+		System.out.println(t.isBool());
 	}
 
-	public void sort(int[] data , int l , int r) {
-		int left = l;
-		int right = r;
-		int pivot = data[(l+r) / 2];
+	public boolean isBool(){
+		String str = "";
 
-		do {
-			while(data[left] < pivot) left++;
-			while(data[right] > pivot) right--;
-			if(left <= right) {
-				int temp = data[left];
-				data[left] = data[right];
-				data[right] = temp;
-				left++;
-				right--;
-			}
-		}while(left <= right);
-		if(l < right) sort(data , l ,right);
-		if(r > left) sort(data , left , r);
+		return (str == null || "".equals(str));
 	}
 }
