a005bea86449a0274111eed35bc6098810f0e26f
drogba <drogba02@naver.com>
2020-05-27 18:51:18 +0900

    N찍기 No_2741

---
+++ src/backjoon/No_2741.java
@@ -0,0 +1,13 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class No_2741 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for(int i=1; i<=a; i++) {
+            System.out.println(i);
+        }
+    }
+}
