57b77bffebd151469b93736ef1ce960c420e4a78
drogba <drogba02@naver.com>
2020-05-28 21:32:32 +0900

    기찍N No_2742

---
+++ src/backjoon/No_2742.java
@@ -0,0 +1,13 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class No_2742 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for(int i=a; i>0; i--) {
+            System.out.println(i);
+        }
+    }
+}
