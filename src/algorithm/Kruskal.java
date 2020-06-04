41a9bc085f9ab80d851ce44993b24e893282b4c7
drogba <drogba02@naver.com>
2020-06-04 09:18:30 +0900

    크루스칼 알고리즘 스터디

--- src/algorithm/Kruskal.java
+++ src/algorithm/Kruskal.java
@@ -1,57 +1,87 @@
 package algorithm;
 
-import sun.jvm.hotspot.oops.Array;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Vector;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 // 가장 적은 비용으로 모든 노드를 연결 (사이클이 발생하면 안됨)
+
+
+class A implements Comparable<A> {
+    int s;
+    int e;
+    int v;
+    public A(int s , int e , int v) {
+        super();
+        this.s = s;
+        this.e = e;
+        this.v = v;
+    }
+
+    @Override
+    public int compareTo(A o) { // min heap 을 만들기 위한 우선순위 큐용 Comparable 메서드
+        return o.v >= this.v ? -1 : 1;
+    }
+}
+
 public class Kruskal {
-    // 부모 노드를 찾는 함수
-    int getParent(int parent[] , int x) {
-        if(parent[x] == x) return x;
-        return parent[x] = getParent(parent , parent[x]);
+    static int N; // 정점의 개수
+    static int E; // 간선의 개수
+    static PriorityQueue<A> pq; // 간선 값을 Min Heap 으로 하는 우선순위 큐
+    static int[] parent;    // union find 에 필요한 부모 노드를 저장하는 배열
+    static boolean[] visit; // 방문 여부 배열
+    static int result; // 결과 값 저장
+
+    public static int find(int a) {
+        if (a == parent[a]) return a; // 초기화된 상태(정점이 처음 등장) 이면 자기 자신이 부모
+        parent[a] = find(parent[a]); // find 할때마다 부모는 최상위 부모로 설정 (재귀로 구현)
+        return parent[a];
     }
 
-    // 두 부모 노드를 합치는 함수
-    int unionParent(int parent[] , int a , int b) {
-        a = getParent(parent , a);
-        b = getParent(parent , b);
-        if(a < b) {
-            return parent[b] = a;
+    public static void union(int a , int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if(aRoot != bRoot) {
+            parent[aRoot] = b;
         } else {
-            return parent[a] = b;
+            return;
         }
     }
 
-    // 같은 부모를 가지는지 확인
-    int findParent(int parent[] , int a , int b) {
-        a = getParent(parent , a);
-        b = getParent(parent , b);
-        if (a == b) {
-            return 1;
-        } else {
-            return 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        N = Integer.valueOf(br.readLine()); // 노드의 개수
+        E = Integer.valueOf(br.readLine()); // 간선의 개수
+
+        parent = new int[N+1]; // Disjoint-set
+        visit = new boolean[N+1];
+        result = 0;
+
+        pq = new PriorityQueue<A>();
+        String[] tempStr;
+        for(int i=0; i<E; i++) {
+            tempStr = br.readLine().split(" ");
+            pq.add(new A(Integer.valueOf(tempStr[0]) , Integer.valueOf(tempStr[1]) , Integer.valueOf(tempStr[2])));
+        } // 모든 간선에 대해 [시작 , 끝 , 비용] 을 가진 클래스로 우선순위 큐에 add
+
+        for(int i=1; i<=N; i++) {
+            parent[i] = i;
+        } // union-find 의 초기화는 일단 자기 자신의 부모노드가 자기 자신으로 설정
+
+        for(int i=0; i<E; i++) { // 모든 간선에 대해 확인
+            A oneNode = pq.poll(); // 현재 큐에 있는 모든 인스턴스중 비용이 가장 적은 간선이 poll 된다.
+            int start = oneNode.s;
+            int end = oneNode.e;
+            int a = find(start);  // 만약 이 간선을 선택해서 연결한다 했을때 사이클이 생기면 안되므로
+            int b = find(end);  // 양쪽의 루트노드가 무엇인지 확인하고
+            if (a == b) continue; // 만약 같으면 선택하지 않고 넘어간다.
+
+            union(start , end);
+            result += oneNode.v;
         }
-    }
-
-    // 간선 정보를 갖는 클래스 생성
-    static class Edge {
-        public int node[] = new int[2];
-        public int distance;
-        Edge(int a , int b , int distance) {
-            this.node[0] = a;
-            this.node[1] = b;
-            this.distance = distance;
-        }
-
-    }
-
-    public static void main(String[] args) {
-        Kruskal kruskal = new Kruskal();
+        System.out.println(result);
     }
 }
 
--- src/test/test.java
+++ src/test/test.java
@@ -7,32 +7,6 @@ import java.util.stream.Collectors;
 
 public class test {
 	public static void main(String[] args) {
-		TestVO tvo = new TestVO();
-		tvo.setName("드록바");
-		tvo.setPhone("samsung");
-		tvo.setNumber(32431L);
-		TestVO tvo2 = new TestVO();
-		tvo2.setName("BBB");
-		tvo2.setPhone("apple");
-		tvo2.setNumber((long) 22534);
-		TestVO tvo3 = new TestVO();
-		tvo3.setName("CCC");
-		tvo3.setPhone("samsung");
-		tvo3.setNumber((long) 100);
-
-
-		List<TestVO> tlist = new ArrayList<>();
-		tlist.add(tvo);
-		tlist.add(tvo2);
-		tlist.add(tvo3);
-
-		Map<Long, List<TestVO>> screeningPartnerMap = tlist.stream().collect(Collectors.groupingBy(TestVO::getNumber));
-//		System.out.println(screeningPartnerMap.toString());
-
-		for(Long key : screeningPartnerMap.keySet()) {
-			System.out.println(key);
-			System.out.println(screeningPartnerMap.get(key));
-		}
-
+		
 	}
 }
