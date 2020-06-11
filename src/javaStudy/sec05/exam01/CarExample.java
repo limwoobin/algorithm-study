60450273c1c06bb76b6309e9305145576c377216
drogba <drogba02@naver.com>
2020-06-11 20:42:15 +0900

    static , instance 스터디

---
+++ src/javaStudy/sec05/exam01/CarExample.java
@@ -0,0 +1,11 @@
+package javaStudy.sec05.exam01;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car("아반떼");
+        Car yourCar = new Car("K5");
+
+        myCar.run();
+        yourCar.run();
+    }
+}
---
+++ src/javaStudy/sec05/exam02/Calculator.java
@@ -0,0 +1,14 @@
+package javaStudy.sec05.exam02;
+
+public class Calculator {
+    int pp = 5;
+    static double pi = 3.14159;
+
+    static int plus(int x , int y) {
+        return x + y;
+    }
+
+    static int minus(int x , int y) {
+        return x - y;
+    }
+}
