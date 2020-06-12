7b965428c40bac7a7fdb9c9c799bbb29bca817e7
drogba <drogba02@naver.com>
2020-06-12 20:52:11 +0900

    다형성

---
+++ src/javaStudy/sec02/exam02/Child.java
@@ -0,0 +1,13 @@
+package javaStudy.sec02.exam02;
+
+public class Child extends Parent {
+
+    @Override
+    public void method2() {
+        System.out.println("CHild method2");
+    }
+
+    public void method3() {
+        System.out.println("Child method3");
+    }
+}
---
+++ src/javaStudy/sec02/exam02/ChildExample.java
@@ -0,0 +1,11 @@
+package javaStudy.sec02.exam02;
+
+public class ChildExample {
+    public static void main(String[] args) {
+        Child child = new Child();
+
+        Parent parent = child;
+        parent.method1();
+        parent.method2();
+    }
+}
