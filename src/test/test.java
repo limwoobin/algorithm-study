bc27a2c0ce7f94c504f744236d386ae34e649144
drogba <drogba02@naver.com>
2020-06-16 20:31:33 +0900

    예외 처리

--- src/test/test.java
+++ src/test/test.java
@@ -1,13 +1,19 @@
 package test;
 
-
-import javaStudy.sec02.exam04.Driver;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
 public class test {
 	public static void main(String[] args) {
-		
+		String[] arr = {"10" , "a10"};
+		int value = 0;
+		for(int i=0; i<=2; i++) {
+			try {
+				value = Integer.parseInt(arr[i]);
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("인덱스 초과");
+			} catch (NumberFormatException e) {
+				System.out.println("숫자변환 불가");
+			} finally {
+				System.out.println(value);
+			}
+		}
 	}
 }
