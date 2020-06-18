f4c96b5aa6298b962f2a1bc93ca2414e56d8e777
drogba <drogba02@naver.com>
2020-06-18 20:15:40 +0900

    Math 함수

---
+++ src/javaStudy/sec12/exam08/MathRandomExample.java
@@ -0,0 +1,8 @@
+package javaStudy.sec12.exam08;
+
+public class MathRandomExample {
+    public static void main(String[] args) {
+        int lotto = (int)(Math.random() * 45) + 1;
+        System.out.println(lotto);
+    }
+}
