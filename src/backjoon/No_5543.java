8b223ad0a2ad4ba8f55be9ff826dce6c4c6cf793
drogba <drogba02@naver.com>
2020-06-30 18:40:08 +0900

    상근날드 No_5543

---
+++ src/backjoon/No_5543.java
@@ -0,0 +1,33 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class No_5543 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[] arr = new int[6];
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int e = in.nextInt();
+
+        arr[0] = a + d;
+        arr[1] = a + e;
+        arr[2] = b + d;
+        arr[3] = b + e;
+        arr[4] = c + d;
+        arr[5] = c + e;
+
+        int min = arr[0];
+        for(int i=0; i<arr.length; i++) {
+            if(min > arr[i]) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println(min-50);
+    }
+}
