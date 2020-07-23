a0b1e07cdce084c71489716022545525c0907f1e
drogba <drogba02@naver.com>
2020-07-23 18:38:06 +0900

    고양이 No_10171

---
+++ src/backjoon/No_10171.java
@@ -0,0 +1,10 @@
+package backjoon;
+
+public class No_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
