1aa0f9089d2387aa0511b89d6546a7f5804fa428
drogba <drogba02@naver.com>
2020-09-09 18:32:29 +0900

    No_10951 A+B-4

---
+++ src/backjoon/No_10951.java
@@ -0,0 +1,15 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class No_10951 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while(in.hasNextInt()) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
