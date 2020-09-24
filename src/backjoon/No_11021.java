b1dda5edaf5763c6baeed709c9da12480526af88
limwoobin <drogba02@naver.com>
2020-09-25 00:32:11 +0900

    No_11021 A+B-7

---
+++ src/backjoon/No_11021.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class No_11021 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = in.nextInt();
+
+        for(int i=1; i<=count; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println("Case #"+i+": " + (a+b));
+        }
+    }
+}
