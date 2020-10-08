d54ba660100a89a6e2627d268c6621ab9f1a2f4c
drogba <drogba02@naver.com>
2020-10-08 18:04:00 +0900

    아스키코드 No_11654

---
+++ src/backjoon/No_11654.java
@@ -0,0 +1,11 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class No_11654 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        System.out.println(a.hashCode());
+    }
+}
