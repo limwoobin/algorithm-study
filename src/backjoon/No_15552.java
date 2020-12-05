3706ea4f430f78387a5a3c680683161449fd29d3
limwoobin <drogba02@naver.com>
2020-12-05 12:57:15 +0900

    No_15552
    빠른 A + B

---
+++ src/backjoon/No_15552.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.*;
+
+public class No_15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        for(int i=0; i<count; i++) {
+            String[] temp = br.readLine().split(" ");
+            bw.write(Integer.parseInt(temp[0])+Integer.parseInt(temp[1]) + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
